6353b590928259bdd0314f8515c34402d4b391ec
Ayza <87999011+Ayza69420@users.noreply.github.com>
2022-03-16 10:42:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+
+start_time = time.time()
+sentence = "this is what you gotta write to test your wpm."
+
+inp = input(f">> {sentence}\n")
+
+correct = 0
+
+for i,v in enumerate(sentence):
+    try:
+        if inp[i] == v:
+            correct += 1
+    except Exception:
+        break
+
+time_taken = time.time()-start_time
+
+print(f"WPM: {len(inp)*60/(5*time_taken)}\nAcurracy: {correct/len(sentence)*100}%")
